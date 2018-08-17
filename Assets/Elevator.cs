840a24620453893fa5604dde7036fad2c407bfcd
Neill <neillbogie@googlemail.com>
2018-08-17 02:33:29 +0100

    Add rings scene with elevators (jump zones)

---
+++ Assets/Elevator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+                   
+public class Elevator : MonoBehaviour {
+    private RigidbodyFirstPersonController controller;
+    [SerializeField]
+    private float jumpForceExtra;
+    [SerializeField]
+    private float jumpForceNormal;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        controller = other.GetComponent<RigidbodyFirstPersonController>();
+        if (controller)
+        {
+            controller.movementSettings.JumpForce = jumpForceExtra;
+            controller.advancedSettings.airControl = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        controller = other.GetComponent<RigidbodyFirstPersonController>();
+        if (controller)
+        {
+            controller.movementSettings.JumpForce = jumpForceNormal;
+            controller.advancedSettings.airControl = false;
+        }
+    }
+
+}
